e93ca469b3fc4a1de33705c2f01bf87b1724c29d
Yuqicheng Zhu <zhuyuqicheng@gmail.com>
2023-02-01 00:19:05 +0100

    NF1 finish

--- NF1.py
+++ NF1.py
@@ -40,16 +40,53 @@ def single_class_result(sparql, cls_str):
         if (cls_str != x["subclass"]["value"]) & (numerator/denominator<1.0):
             result.append((cls_str, x["subclass"]["value"], numerator/denominator))
     return pd.DataFrame(result, columns=["ConceptA", "ConceptB", "Probability"])
-
-#%%
-if __name__ == "__main__":
-    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-    sparql.setReturnFormat(JSON)
+def type1_hierarchy(sparql):
     loop_class = get_loop_class(sparql)
     result = []
     for cls in loop_class:
         result.append(single_class_result(sparql, cls))
         print(cls)
     result_pd = pd.concat(result, ignore_index=True)
-    result_pd.to_csv("NF1.csv", index=False)
+    result_pd.to_csv("NF1_hier.csv", index=False)
+
+def count_subclass(sparql, cls):
+    query = """SELECT DISTINCT ?subclass (COUNT(?instance) AS ?number) """
+    query += f" WHERE{{?subclass rdfs:subClassOf <{cls}> ."
+    query += f" ?instance a ?subclass}}"
+    sparql.setQuery(query)
+    ret = sparql.queryAndConvert()
+    result = dict()
+    for x in ret["results"]["bindings"]:
+        result[x["subclass"]["value"]] = x["number"]["value"]
+    return result
+def single_intersection(sparql, cls):
+    denom = count_subclass(sparql, cls)
+    query = """SELECT DISTINCT ?subclass1 ?subclass2 (COUNT(?instance) AS ?number) """
+    query += f" WHERE{{?subclass1 rdfs:subClassOf <{cls}> ."
+    query += f" ?subclass2 rdfs:subClassOf <{cls}> ."
+    query += f" ?instance a ?subclass1 . ?instance a ?subclass2 ."
+    query += f" FILTER (?subclass1 != ?subclass2)}}"
+    sparql.setQuery(query)
+    ret = sparql.queryAndConvert()
+    result = []
+    for x in ret["results"]["bindings"]:
+        p = int(x["number"]["value"])/int(denom[x["subclass1"]["value"]])
+        result.append((x["subclass1"]["value"], x["subclass2"]["value"], p))
+    return pd.DataFrame(result, columns=["ConceptA", "ConceptB", "Probability"])
+def type1_intersection(sparql):
+    loop_class = get_loop_class(sparql)
+    result = []
+    for cls in loop_class:
+        result.append(single_intersection(sparql, cls))
+        print(cls)
+    result_pd = pd.concat(result, ignore_index=True)
+    result_pd.to_csv("NF1_inter.csv", index=False)    
+
+#%%
+if __name__ == "__main__":
+    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+    sparql.setReturnFormat(JSON)
+
+    type1_intersection(sparql)
+    
 # %%
